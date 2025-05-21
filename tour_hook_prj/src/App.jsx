ab4c8eea7f02314c597f23d4ff3df6b338fe37fe
Sagar <rohit28sagar@gmail.com>
2025-05-21 22:29:52 +0530

    react4

--- tour_hook_prj/src/App.jsx
+++ tour_hook_prj/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Tours from './components/tours'
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-     
+     <Tours/>
     </>
   )
 }
