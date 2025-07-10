8a4458135259c48be18e4db6e1264b2610b561fe
Sagar <rohit28sagar@gmail.com>
2025-07-10 16:23:31 +0530

    react_useEffect3

--- hook_useEffect_prj/src/App.jsx
+++ hook_useEffect_prj/src/App.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-import './App.css'
+import Navbar from "./components/Navbar";
+import Filter from "./components/filter";
+import Cards from "./components/Cards";
+import { apiUrl,filterData } from "./data/data";
 
 function App() {
+ 
+  const[courses,Setcourses]=useState(null);
+  const[loading,Setloading]=useState(true);
+
+  async function getData(params)
+  {
+    Setloading(true);
+     try {
+         const res = await fetch (apiUrl);
+         const output = await res.json();
+         Setcourses(output);
+         
+     } catch (error) {
+       console.log("Error is there");
+       
+     }
+     Setloading(false);
+  }
+
+  useEffect(()=>{
+    getData();
+  },[]);
+
 
 
   return (
-    <>
-        
-    </>
-  )
+  <>
+      <div>
+       <Navbar/>
+       <Filter filterData={filterData} />
+       <Cards courses={courses}/>
+      </div>
+  </>
+  );
 }
 
-export default App
+export default App;
